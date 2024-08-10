1da9c34d772b558572ed0887de047ef23da62ef6
seunggi <hsk1533@naver.com>
2024-08-10 16:19:35 +0900

    branch: view setup

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -6,28 +6,20 @@ export default function CameraScreen() {
   return (
     <View style={styles.container}>
       <Text style={{ color: 'white' }}>CameraScreen</Text>
-      <View
-        style={{
-          backgroundColor: 'white',
-          width: '80%',
-          height: '50%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: 5,
-          margin: 10,
-        }}
-      >
+      <View style={styles.preview}>
         <Text>Preview Vedio</Text>
       </View>
       <View style={{ flexDirection: 'row' }}>
         <TouchableOpacity style={styles.btn}>
-          <FontAwesome name="camera" size={30} />
+          <FontAwesome name="video-camera" size={30} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.btn}>
-          <FontAwesome name="upload" size={30} />
+          <FontAwesome name="photo" size={30} />
         </TouchableOpacity>
       </View>
+      <TouchableOpacity style={[styles.btn, { width: 220 }]}>
+        <FontAwesome name="upload" size={30} />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -42,13 +34,24 @@ const styles = StyleSheet.create({
   },
 
   btn: {
-    width: 50,
+    width: 100,
     height: 50,
     backgroundColor: 'white',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,
+    margin: 10,
+  },
+
+  preview: {
+    backgroundColor: 'white',
+    width: '80%',
+    height: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
     margin: 10,
   },
 })
